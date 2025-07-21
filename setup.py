59b48d106e710d79f828a89d794b155991bef3c8
Donggeon Lee <dg981001@naver.com>
2025-07-21 16:47:23 +0900

    bump to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="gVfP",
-    version="0.0.1",
+    version="0.0.2",
+    python_requires='>=3.13',
     packages=find_packages(),
     install_requires=[
         "numpy",
