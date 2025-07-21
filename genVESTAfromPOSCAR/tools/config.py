955a30a56db7da4a8997cee8dc52064f7b46082d
Donggeon Lee <dg981001@naver.com>
2025-07-21 16:46:54 +0900

    add config updator

--- genVESTAfromPOSCAR/tools/config.py
+++ genVESTAfromPOSCAR/tools/config.py
@@ -1,20 +1,43 @@
 import configparser
 
+default_target_file = ""
+default_color_list_file = ""
+default_result_name = ""
+version_ = "0.0.2"
 
-config_base = """
+def config_updator(options=None, for_update=False):
+    target_file = default_target_file
+    color_list_file = default_color_list_file
+    result_name = default_result_name
+    version = version_
+    if for_update:
+        target_file = options["target file"]
+        try:
+            color_list_file = options["color list file"]
+        except:
+            color_list_file = ""
+        result_name = options["result name"]
+        
+    config_base = """
 [Data Info]
 # target file : path of target file.  ex) POSCAR or Dataset/POSCAR # Default(blank) is POSCAR
-target file = 
+target file = {target_file}
 
 [Color Setting]
 # color list file : path of color list file. Default(blank) is blank. It will set color randomly
 # IMPORTANT : Number of colors and number of atoms should be a same number
-color list file = 
+color list file = {color_list_file}
 
 [Result File Option]
-$ result name : setting name of output file. # Default(blank) is POSCAR.vesta
-result name = 
-"""
+# result name : setting name of output file. # Default(blank) is POSCAR.vesta
+result name = {result_name}
+
+[Version]
+version = {version}
+""".format(target_file=target_file, color_list_file=color_list_file, result_name=result_name, version=version)
+
+    return config_base
+
 
 def config_loader(config):
     options = dict()
@@ -34,13 +57,25 @@ def config_is_exist():
     config = configparser.ConfigParser(inline_comment_prefixes="#")
     try:
         config.read("gVfP_config.ini")
-        config.get("Data Info", "target file")
-        options = config_loader(config)
-        return options
+
     except:
         with open('gVfP_config.ini', 'w', encoding='utf-8') as configfile:
-            configfile.write(config_base)
+            configfile.write(config_updator())
         print("Config file is created : gVfP_config.ini")
         print("Set options and then run this script")
         
         exit()
+        
+    try:
+        version_info = config.get("Version", "version")
+        version_match = version_ != version_info
+    except:
+        version_match = False
+    options = config_loader(config)
+    if version_match:
+        with open('gVfP_config.ini', 'w', encoding='utf-8') as configfile:
+            configfile.write(config_updator(options, for_update=True))
+        print("config file has been updated.")
+        print("Please check configurations.")
+        exit()
+    return options
